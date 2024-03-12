c4e19e7cc6febc2d4474b50879fd79ca80fd91b0
priyankakatode18 <priyankamisal1805@gmail.com>
2024-03-12 22:10:43 +0530

    program of bank deatails using function

---
+++ 03_funAssign02.js
@@ -0,0 +1,19 @@
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log("  Bank Name        :CITI Bank" );
+    console.log("Account Number     :3456782345 ");
+    console.log("City               :Pune");
+    console.log("Pin Code           :431202");
+    console.log("=============================================")
+    console.log("  Bank Name        :AXIS Bank" );
+    console.log("Account Number     :7856782345 ");
+    console.log("City               :Mumbai");
+    console.log("Pin Code           :441202");
+    console.log("=============================================")
+    console.log("  Bank Name        :HDFC Bank" );
+    console.log("Account Number     :8956782345 ");
+    console.log("City               :Pune");
+    console.log("Pin Code           :631202");
+    console.log("=============================================")
+    
+    
+    }bankDetails()
