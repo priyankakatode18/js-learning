78fa82a80607a0cf05f5cfa7f0b23bfd6356233e
priyankakatode18 <priyankamisal1805@gmail.com>
2024-03-18 22:41:23 +0530

    07_grade assignment

---
+++ 07_grade.js
@@ -0,0 +1,37 @@
+function gradeCalculation(marks){
+if ( marks==0 || marks>100 || isNaN(+marks) ){
+    console.log(`Please provide the valid marks: ${marks} `)
+}
+else if (marks>=90) {
+    console.log(`Fantastic marks : ${marks}, Your grade is A+`);
+}
+
+else if (marks>=75 && marks<90) {
+    console.log(`Excellent marks ${marks}, your grade is A`);
+    
+} 
+
+else if (marks>=50 && marks<75) {
+    console.log(`Good marks ${marks}, your grade is B`);
+    
+}
+else if (marks>=35 && marks<50) {
+    console.log(`Marks is ${marks}, your grade is C, Need improvement`);  
+}    
+}
+gradeCalculation(98);
+gradeCalculation(80);
+gradeCalculation(90);
+gradeCalculation(0);
+gradeCalculation(150);
+gradeCalculation(-7);
+gradeCalculation(35) ;
+gradeCalculation(29);
+gradeCalculation(64);
+gradeCalculation(49);
+gradeCalculation("92");
+gradeCalculation("Eighty");
+gradeCalculation(undefined);
+gradeCalculation(null) ;
+
+
