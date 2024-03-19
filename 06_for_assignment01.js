521f78cbc227fa5c646f1dcd435b82eda974424e
priyankakatode18 <priyankamisal1805@gmail.com>
2024-03-19 15:54:12 +0530

    06_for_assignment01  js

---
+++ 06_for_assignment01.js
@@ -0,0 +1,45 @@
+console.log(`WAP to print numbers from 5 to 15 by incrementing 1`);
+for (let i = 5; i <=15; i++) {
+    console.log(i);
+}
+console.log(`===================================================================`)
+console.log(`WAP to print numbers from 50 to 40 by decrementing 1`);
+for (let i =50; i>=40; i--) {
+    console.log(i);
+    
+}
+console.log(`WAP to find first 15 odd numbers`);
+for (let index = 1; index<=30; index++) {
+if (index %2!=0) 
+{
+    console.log(index);
+
+}
+}
+console.log(`WAP to find first 10 even numbers`);
+for (let index = 0; index <20; index++) {
+    if (index % 2 ==0){
+        console.log(index);
+    }
+  
+    
+}
+
+
+console.log(`WAP to print table 5 like.....5 10 15 20  25 30 .....50`);
+for(let i=5; i<=50; i+=5)
+{
+    console.log(i);
+}
+
+console.log(`WAP to print table 10 like.. 10 20  30 .....100`);
+for (let i=10;i<=100;i+=10){
+    console.log(i);
+}
+
+console.log(`WAP to print table 10 reverse  like.. 100 99 98 97...10`);
+for (let i=100; i>=91; i--)
+{
+    console.log(i);
+}
+
